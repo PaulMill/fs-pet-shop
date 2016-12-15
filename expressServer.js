9dbe57603d0fc68165cd8a072da89d5a7402e886
PaulMill <usa.paul@icloud.com>
2016-12-15 13:31:08 -0800

    assignment complete

---
+++ expressServer.js
@@ -0,0 +1,98 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const petsPath = path.join(__dirname, 'pets.json');
+
+const express = require('express');
+const app = express();
+
+app.disable('x-powered-by');
+
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/pets', (req, res, next) => {
+  fs.readFile(petsPath, 'utf8', (readErr, petsJSON) => {
+    if (readErr) {
+      return next(readErr);
+    }
+    const pets = JSON.parse(petsJSON);
+
+    res.send(pets);
+  });
+});
+
+app.post('/pets', (req, res, next) => {
+  fs.readFile(petsPath, 'utf8', (readErr, petsJSON) => {
+    if (readErr) {
+      return next(readErr);
+    }
+
+    const pets = JSON.parse(petsJSON);
+    const name = req.body.name;
+    const kind = req.body.kind;
+    const age = Number.parseInt(req.body.age);
+
+    if (name && kind && age) {
+      const newPet = { age, kind, name };
+
+      pets.push(newPet);
+      const newPetsJSON = JSON.stringify(pets);
+
+      fs.writeFile(petsPath, newPetsJSON, (writeErr) => {
+        if (writeErr) {
+          return next(writeErr);
+        }
+        res.send(newPet);
+      });
+    }
+    else {
+      return res.sendStatus(400);
+    }
+  });
+});
+
+app.get('/pets/:id', (req, res, next) => {
+  fs.readFile(petsPath, 'utf8', (readErr, petsJSON) => {
+    if (readErr) {
+      return next(readErr);
+    }
+    const id = Number.parseInt(req.params.id);
+    const pets = JSON.parse(petsJSON);
+
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+    res.send(pets[id]);
+  });
+});
+
+// function for catching errors 500
+app.use((err, _req, res, _next) => {
+  // eslint-disable-next-line no-console
+  console.error(err.stack);
+
+  return res.sendStatus(500);
+});
+
+app.get('/boom', (_req, _res, next) => {
+  next(new Error ('BOOM!!!'));
+});
+
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Listening on port ${port}`);
+});
+
+module.exports = app;
